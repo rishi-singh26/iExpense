996752b0bd7c942e88b3a7209ffc7907e373e73d
rishi-singh26 <rishisingh0831@gmail.com>
2023-09-11 14:50:26 +0530

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Rishi Singh on 11/09/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
